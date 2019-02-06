95dba011f316af3b2c963fa8451b370dfc030377
mishashka <misganbold@ucdavis.edu>
2019-02-06 11:53:21 -0800

    update

--- studio2/script.js
+++ studio2/script.js
@@ -1,16 +1,27 @@
 'use strict';
 console.log('reading js');
-//create a hover effect
-var airpod=document.getElementById('airpod');
-var lip=document.getElementById('lip');
-var mint=document.getElementById('mint');
+//global variables
+var airpod = document.querySelector('#airpod');
+var overlay = document.querySelector('#overlay');
+var p = document.querySelector('#overlay p');
+var close = document.querySelector('#close');
 
-airpod.addEventListener('mouseover',function()){
+var lip = document.querySelector('#lip');
+var mint = document.querySelector('#mint');
 
-}
-lip.addEventListener('mouseover',function()){
+airpod.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    p.innerHTML='I just bought Rocket airpods and they got lost in the mail';
+})
+lip.addEventListener('click', function() {
+    overlay.style.display = 'block';
+      p.innerHTML='I just bought Rocket airpods and they got lost in the mail';
+})
+mint.addEventListener('click', function() {
+    overlay.style.display = 'block';
+      p.innerHTML='I just bought Rocket airpods and they got lost in the mail';
+})
 
-}
-mint.addEventListener('mouseover',function()){
-
-}
+close.addEventListener('click', function() {
+    overlay.style.display = "none";
+})
