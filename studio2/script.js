f090823442232196eb89fe86626fecfe83e6ee1e
mishashka <misganbold@ucdavis.edu>
2019-02-06 09:03:09 -0800

    studio2

---
+++ studio2/script.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('reading js');
+//create a hover effect
+var airpod=document.getElementById('airpod');
+var lip=document.getElementById('lip');
+var mint=document.getElementById('mint');
+
+airpod.addEventListener('mouseover',function()){
+
+}
+lip.addEventListener('mouseover',function()){
+
+}
+mint.addEventListener('mouseover',function()){
+
+}
