b9dde798bd21124b03daf7310e2f818ae2e017c0
mishashka <misganbold@ucdavis.edu>
2019-03-16 08:21:14 -0700

    final code version1

--- process/development/proto2.js
+++ process/development/proto2.js
@@ -1,5 +1,6 @@
 'use strict';
 console.log('reading js');
+var itemCount = 0;
 var pages = document.querySelectorAll(".page");
 
 var banana = document.querySelector('#banana');
@@ -36,19 +37,36 @@ var shirtSide = document.querySelector('#shirtSide');
 
 var sideBar = document.querySelector('#sidebar');
 
+ function goHome(currentBedroom){
+   window.location.reload()
 
-function myFunction() {
-    for (let page of pages) {
-        page.addEventListener("click", function(event) {
-            console.log('page');
-            if (event.currentTarget.nextElementSibling.classList.contains("hide")) {
-                event.currentTarget.classList.add("hide");
-                event.currentTarget.nextElementSibling.classList.remove("hide");
-                console.log('displaying', event.currentTarget.nextElementSibling.id);
+}
 
-            }
-        });
+function myFunction(button) {
+    //console.log("this me", button.parentNode.id);
+    let hidePageID;
+    console.log(button);
+    let stringVersion = "" + button;
+    console.log(stringVersion);
+
+    if ( stringVersion.includes("Button") ) {
+      console.log("it should hit here but javascript might be funky")
+      hidePageID = button.parentNode.id // Get the id of the parent so we can see which page we're on
+    } else {
+      hidePageID = button.parentNode.parentNode.id // Get the id of parent of the parent
     }
+
+    console.log("new page", hidePageID);
+    let string = "#" + hidePageID;
+    var pageToHide = document.querySelector(string);
+
+    pageToHide.addEventListener("click", function(event) {
+      if (event.currentTarget.nextElementSibling.classList.contains("hide")) {
+          event.currentTarget.classList.add("hide"); // Hide the current page
+          event.currentTarget.nextElementSibling.classList.remove("hide"); // Show the next page
+          console.log('displaying', event.currentTarget.nextElementSibling.id);
+      }
+    });
 }
 
 banana.addEventListener('click', playBanana);
@@ -57,20 +75,29 @@ macbook.addEventListener('click', playMacbook);
 plant.addEventListener('click', playPlant);
 shirt.addEventListener('click', playShirt);
 
+function checkIfDone() {
+  itemCount += 1
+  if(itemCount == 5){
+    //alert("Hi you're done");
+    console.log("hi do we get inside the if statement")
+    // Maung Notes: Make sure to style & create the alert box as you want and center it or something and if you want a button on it, makesure to have a <button onclick = > or something like that
+    bedroom.innerHTML += '<div class="side1"> <img src = "images/banana.svg" width = "60" alt="banana" id="bananaSide" class="show, grow" /></div>'
+  }
+}
+
 function playBanana() {
     console.log('click on banana');
     bananaInfo.classList.replace('hide', 'show');
     bananaClose.addEventListener('click', function() {
         bananaInfo.classList.replace('show', 'hide');
         banana.classList.replace('show', 'hide');
+        checkIfDone();
         if (!bananaClick){
         sideBar.innerHTML += '<div class="side1"> <img src = "images/banana.svg" width = "60" alt="banana" id="bananaSide" class="show, grow" /></div>'
         bananaClick = true;
       }
       document.querySelector('#bananaSide').addEventListener('click',playBanana);
-      // check how many images are in the sideBar
-      // var sideBarImgs = document.querySelectorAll('#sidebar img')
-      // if (sideBarImgs.length == 5) {console.log('show finish screen')};
+
     });
 }
 
@@ -80,6 +107,7 @@ function playJournal() {
     journalClose.addEventListener('click', function() {
         journalInfo.classList.replace('show', 'hide');
         journal.classList.replace('show', 'hide');
+        checkIfDone();
         if (!journalClick){
           sideBar.innerHTML += '<div class="side2"> <img src="images/journal.svg" width="70" alt="journal" id="journalSide" class="show , grow" /></div>'
           journalClick = true;
@@ -95,6 +123,7 @@ function playMacbook() {
         macbookInfo.classList.replace('show', 'hide');
         closedMacbook.classList.replace('hide', 'show');
         macbook.classList.replace('show', 'hide');
+        checkIfDone();
         if (!macbookClick){
           sideBar.innerHTML += '<div class="side3"> <img src="images/macbook.svg" width="90" alt="macbook" id="macbookSide" class="show , grow" /></div>'
           macbookClick = true;
@@ -109,6 +138,7 @@ function playPlant() {
     plantClose.addEventListener('click', function() {
         plantInfo.classList.replace('show', 'hide');
         plant.classList.replace('show', 'hide');
+        checkIfDone();
         if (!plantClick){
           sideBar.innerHTML += '<div class="side4"> <img src="images/plant.svg" width="60" alt="plant" id="plantSide" class="show , grow" /></div>'
           plantClick = true;
@@ -124,6 +154,7 @@ function playShirt(){
         shirtInfo.classList.replace('show', 'hide');
         closet.classList.replace('hide', 'show');
         shirt.classList.replace('show', 'hide');
+        checkIfDone();
         if (!shirtClick){
           sideBar.innerHTML +='<div class="side5"> <img src="images/shirt.svg" width="90" alt="shirt" id="shirtSide" class="show , grow" /></div>'
           shirtClick = true;
@@ -131,48 +162,3 @@ function playShirt(){
         document.querySelector('#shirtSide').addEventListener('click',playShirt);
 });
 }
-// bananaSide.addEventListener('click', function() {
-//     console.log('click on banana');
-//     bananaInfo.classList.replace('hide', 'show');
-//     bananaClose.addEventListener('click', function() {
-//         bananaInfo.classList.replace('show', 'hide');
-//         bananaSide.classList.replace('show', 'hide');
-//     })
-// });
-//
-// journalSide.addEventListener('click', function() {
-//     console.log('click on journal');
-//     journalInfo.classList.replace('hide', 'show');
-//     journalClose.addEventListener('click', function() {
-//
-//         journalInfo.classList.replace('show', 'hide');
-//         journalSide.classList.replace('show', 'hide');
-//     })
-// });
-//
-// macbookSide.addEventListener('click', function() {
-//     console.log('click on laptop');
-//     macbookInfo.classList.replace('hide', 'show');
-//     macbookClose.addEventListener('click', function() {
-//         macbookInfo.classList.replace('show', 'hide');
-//         macbookSide.classList.replace('show', 'hide');
-//     })
-// });
-//
-// plantSide.addEventListener('click', function() {
-//     console.log('click on the plant');
-//     plantInfo.classList.replace('hide', 'show');
-//     plantClose.addEventListener('click', function() {
-//         plantInfo.classList.replace('show', 'hide');
-//         plantSide.classList.replace('show', 'hide');
-//     })
-// });
-//
-// shirtSide.addEventListener('click', function() {
-//     console.log('click on the shirt');
-//     shirtInfo.classList.replace('hide', 'show');
-//     shirtClose.addEventListener('click', function() {
-//         shirtInfo.classList.replace('show', 'hide');
-//         shirtSide.classList.replace('show', 'hide');
-//     })
-// });
